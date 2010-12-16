e194ed2fb34d0af96be728d8487c66b35aa2241c
Jussi Savolainen <ajussis@gmail.com>
2010-12-16 13:38:26 +0000

    browser configure.zcml and viewlets.py updated

--- cip.cipotato/src/cip.cipotatotheme/cip/cipotatotheme/browser/viewlets.py
+++ cip.cipotato/src/cip.cipotatotheme/cip/cipotatotheme/browser/viewlets.py
@@ -1,6 +1,15 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from datetime import date
+from Acquisition import aq_base, aq_inner
+from zope.component import getMultiAdapter
+from Products.CMFCore.utils import getToolByName
+from plone.memoize.view import memoize
+from urllib import unquote
+from plone.app.layout.viewlets import common
+from plone.app.layout.globals.interfaces import IViewView
+from zope.interface import implements, alsoProvides
+
 
 # Sample code for a basic viewlet (In order to use it, you'll have to):
 # - Un-comment the following useable piece of code (viewlet python class).
@@ -27,3 +36,104 @@ class FooterViewlet(ViewletBase):
 
     def update(self):
         self.year = date.today().year
+
+class ContentViewsViewlet(ViewletBase):
+    index = ViewPageTemplateFile('templates/contentviews.pt')
+
+    @memoize
+    def prepareObjectTabs(self, default_tab='view', sort_first=['folderContents']):
+        """Prepare the object tabs by determining their order and working
+        out which tab is selected. Used in global_contentviews.pt
+        """
+        context = aq_inner(self.context)
+        context_url = context.absolute_url()
+        context_fti = context.getTypeInfo()
+
+        context_state = getMultiAdapter(
+            (context, self.request), name=u'plone_context_state'
+        )
+        actions = context_state.actions
+
+        action_list = []
+        if context_state.is_structural_folder():
+            action_list = actions('folder')
+        action_list.extend(actions('object'))
+
+        tabs = []
+        found_selected = False
+        fallback_action = None
+
+        request_url = self.request['ACTUAL_URL']
+        request_url_path = request_url[len(context_url):]
+
+        if request_url_path.startswith('/'):
+            request_url_path = request_url_path[1:]
+        for action in action_list:
+            item = {'title'    : action['title'],
+                    'id'       : action['id'],
+                    'url'      : '',
+                    'selected' : False}
+
+            action_url = action['url'].strip()
+            starts = action_url.startswith
+            if starts('http') or starts('javascript'):
+                item['url'] = action_url
+            else:
+                item['url'] = '%s/%s' % (context_url, action_url)
+
+            action_method = item['url'].split('/')[-1]
+
+            # Action method may be a method alias:
+            # Attempt to resolve to a template.
+            action_method = context_fti.queryMethodID(
+                action_method, default=action_method
+            )
+            if action_method:
+                request_action = unquote(request_url_path)
+                request_action = context_fti.queryMethodID(
+                    request_action, default=request_action
+                )
+                if action_method == request_action:
+                    item['selected'] = True
+                    found_selected = True
+
+            current_id = item['id']
+            if current_id == default_tab:
+                fallback_action = item
+
+            tabs.append(item)
+
+        if not found_selected and fallback_action is not None:
+            fallback_action['selected'] = True
+
+        def sortOrder(tab):
+            try:
+                return sort_first.index(tab['id'])
+            except ValueError:
+                return 255
+
+        tabs.sort(key=sortOrder)
+        return tabs
+
+class PathBarViewlet(common.PathBarViewlet):
+    render = ViewPageTemplateFile('templates/path_bar.pt')
+
+class ContentActionsViewlet(ViewletBase):
+    index = ViewPageTemplateFile('templates/contentactions.pt')
+
+    def update(self):
+        context = aq_inner(self.context)
+        context_state = getMultiAdapter((context, self.request),
+                                        name=u'plone_context_state')
+
+        self.object_actions = context_state.actions('object_actions')
+
+        # The drop-down menus are pulled in via a simple content provider
+        # from plone.app.contentmenu. This behaves differently depending on
+        # whether the view is marked with IViewView. If our parent view
+        # provides that marker, we should do it here as well.
+        if IViewView.providedBy(self.__parent__):
+            alsoProvides(self, IViewView)
+
+    def icon(self, action):
+        return action.get('icon', None)

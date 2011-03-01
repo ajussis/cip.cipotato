799283a06e15c19b130b8830ad0d15248be94522
Jussi Savolainen <ajussis@gmail.com>
2011-03-01 14:13:16 +0000

    images added

---
+++ cip.cipotato/src/cip.cipotatotheme/cip/cipotatotheme/browser/publications.py
@@ -0,0 +1,36 @@
+from Acquisition import aq_inner
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils  import getToolByName
+from Products.CMFPlone.browser.navtree import getNavigationRoot
+from datetime import datetime
+
+class PublicationsView(BrowserView):
+    """Default view of a Project Folder
+    """
+    __call__ = ViewPageTemplateFile('templates/publications.pt')
+
+    def getInfo(self):
+        print m
+        results = self.context.portal_catalog.searchResults(portal_type="Person", getSubsection="CPAD")
+        return results
+
+    def cpadHead(self):
+        result = self.context.portal_catalog.searchResults(portal_type="Person", getSubsection="CPAD", getHead=1)
+        return result[0]
+
+    def dg(self):
+        """
+        Get all the persons working for CPAD
+        """
+        results = self.context.portal_catalog.searchResults(portal_type="Person", getSubsection="DG")
+        return results
+
+    def grants(self):
+        """
+        Get all the persons working for CPAD
+        """
+        #import ipdb
+        #ipdb.set_trace()
+        results = self.context.portal_catalog.searchResults(portal_type="Person", getSubsection="Grants")
+        return results

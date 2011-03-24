c2928f3ed97309c73a15118c03b4ab2bc4399ba9
Jussi Savolainen <ajussis@gmail.com>
2011-03-24 04:33:53 +0000

    publications script fixed

--- cip.cipotato/src/cip.policy/cip/policy/transmogrifierpublications.py
+++ cip.cipotato/src/cip.policy/cip/policy/transmogrifierpublications.py
@@ -4,6 +4,7 @@ from collective.transmogrifier.utils import defaultMatcher
 from BeautifulSoup import BeautifulStoneSoup
 from collective.transmogrifier import utils
 import sys
+import csv
 
 class FormatSetter(object):
 
@@ -159,23 +160,49 @@ class PublicationSource(object):
     def source(self):
         xml = open("/home/ajussis/Develop/cip.cipotato/cip.cipotato/src/cip.policy/cip/policy/pubimport/publications.xml")
         lines = xml.readlines()
+        xml.close()
         xml = '\n'.join(lines[1:])
         soup = BeautifulStoneSoup(xml)
         allPubs = len(soup.findAll('publication'))
         allFields = len(soup.contents[0].contents[1])-1
-        d = {}
+        pubdict = {}
         list = []
         d = 1
         m = 1
-        year
-        #import pdb; pdb.set_trace()
+        year = 1978
+        import pdb; pdb.set_trace()
         for count in range(1, allPubs,2):
             for field in range(1, allFields, 2):
                 if (soup.contents[0].contents[count].contents[field].name == 'year'):
-
-                    d["creation_date"] = d+'/'+m+'/'+year
-                    d = d+1
-                    m = m+1
-                    if d = 28
-                d[soup.contents[0].contents[count].contents[field].name] = soup.contents[0].contents[count].contents[field].string
-            yield d
+                    years = open("/home/ajussis/Develop/cip.cipotato/cip.cipotato/src/cip.policy/cip/policy/pubimport/years.csv", "rt").read()
+                    yearstowrite = open("/home/ajussis/Develop/cip.cipotato/cip.cipotato/src/cip.policy/cip/policy/pubimport/years.csv", "w")
+                    dd = {}
+                    ms = years.split(',')
+                    ms[-1] = ms[-1][:-2]
+                    for i in ms:
+                        dd[i[-4:]]=i.rsplit('/',1)[0]
+                    yearstring = soup.contents[0].contents[count].contents[field].string
+                    try:
+                        yd = dd[yearstring]
+                        d = int(yd.split('/')[0])+1
+                        m = int(yd.split('/')[1])
+                    except:
+                        dd[yearstring] = '1/1'
+                    if (m != 2 and d > 27):
+                        d = 1
+                        m = m + 1
+                    if (m > 11 and d > 26):
+                        m = 1
+                    pubdict["creation_date"] = str(d)+'/'+str(m)+'/'+soup.contents[0].contents[count].contents[field].string
+                    dd[yearstring] = str(d)+'/'+str(m)
+                    ss = {}
+                    try:
+                        fieldnames = dd.keys()
+                        for i in fieldnames:
+                            ss[i] = dd[i]+'/'+i
+                        writer = csv.DictWriter(yearstowrite, fieldnames=fieldnames)
+                        writer.writerow(ss)
+                    finally:
+                        yearstowrite.close()
+                pubdict[soup.contents[0].contents[count].contents[field].name] = soup.contents[0].contents[count].contents[field].string
+            yield pubdict

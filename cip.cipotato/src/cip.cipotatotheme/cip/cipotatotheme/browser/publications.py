75a11af3ecea2a97e523b4580e007989827d7297
Jussi Savolainen <ajussis@gmail.com>
2011-04-04 03:25:48 +0000

    partnership images added

--- cip.cipotato/src/cip.cipotatotheme/cip/cipotatotheme/browser/publications.py
+++ cip.cipotato/src/cip.cipotatotheme/cip/cipotatotheme/browser/publications.py
@@ -42,3 +42,22 @@ class PublicationsSubView(BrowserView):
             return img_id
         else:
             return 0
+
+class PublicationsOneView(BrowserView):
+    """Default view of a Project Folder
+    """
+    __call__ = ViewPageTemplateFile('templates/publications_one.pt')
+
+    def Pubs(self, address):
+#        import pdb; pdb.set_trace()
+        pubpath = (self.context.portal_url.getPortalPath()+'/resources/publications/'+address)
+        return self.context.portal_catalog.searchResults(path = {"query": (pubpath)},portal_type="Publication", sort_on="Date", sort_order="reverse")[:2]
+
+    def ImageExist(self, img_id):
+#        import pdb; pdb.set_trace()
+        img_id = img_id + '-jpg'
+        img = self.context.portal_catalog.searchResults(id=img_id)
+        if img:
+            return img_id
+        else:
+            return 0

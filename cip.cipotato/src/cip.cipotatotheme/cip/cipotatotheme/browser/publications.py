d6c765cdde6d71dedbd112d3258f1eaaf41f91b0
Jussi Savolainen <ajussis@gmail.com>
2011-05-05 14:13:25 +0000

    various changes to frontpage and elsewhere

--- cip.cipotato/src/cip.cipotatotheme/cip/cipotatotheme/browser/publications.py
+++ cip.cipotato/src/cip.cipotatotheme/cip/cipotatotheme/browser/publications.py
@@ -4,6 +4,13 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils  import getToolByName
 from Products.CMFPlone.browser.navtree import getNavigationRoot
 from datetime import datetime
+from zope.interface import implements, Interface
+from zope import schema
+
+from Products.ATContentTypes.interface import IATTopic
+
+#from cip.cipotatotheme import appMessageFactory as _
+
 
 class PublicationsView(BrowserView):
     """Default view of a Project Folder
@@ -24,10 +31,22 @@ class PublicationsView(BrowserView):
         else:
             return 0
 
-class PublicationsSubView(BrowserView):
-    """Default view of a Project Folder
+
+class IPublicationsSubView(Interface):
+    """ Allowed template variables exposed from the view.
     """
-    __call__ = ViewPageTemplateFile('templates/publications_sub.pt')
+
+    # Item list as iterable Products.CMFPlone.PloneBatch.Batch object
+    contents = schema.Object(Interface)
+
+
+
+class PublicationsSubView(BrowserView):
+    """
+    List summary information for all publications in the folder.
+    Batch results.
+    """
+    #__call__ = ViewPageTemplateFile('templates/publications_sub.pt')
 
     def Pubs(self, address):
 #        import pdb; pdb.set_trace()
@@ -42,6 +61,54 @@ class PublicationsSubView(BrowserView):
             return img_id
         else:
             return 0
+
+    implements(IPublicationsSubView)
+
+    def query(self, start, limit, contentFilter):
+        """ Make catalog query for the folder listing.
+
+        @param start: First index to query
+
+        @param limit: maximum number of items in the batch
+
+        @param contentFilter: portal_catalog filtering dictionary with index -> value pairs.
+
+        @return: Products.CMFPlone.PloneBatch.Batch object
+        """
+
+        # Batch size
+        b_size = limit
+
+        # Batch start index, zero based
+        b_start = start
+
+        # We use different query method, depending on
+        # whether we do listing for topic or folder
+        if IATTopic.providedBy(self.context):
+            # ATTopic like content
+            # Call Products.ATContentTypes.content.topic.ATTopic.queryCatalog() method
+            # This method handles b_start internally and
+            # grabs it from HTTPRequest object
+            return self.context.queryCatalog(contentFilter, batch=True, b_size=b_size)
+        else:
+            # Folder or Large Folder like content
+            # Call CMFPlone(/skins/plone_scripts/getFolderContents Python script
+            # This method handles b_start parametr internally and grabs it from the request object
+            contentFilter['sort_order'] = "descending"
+            return self.context.getFolderContents(contentFilter, batch=True, b_size=b_size)
+
+    def __call__(self):
+        """ Render the content item listing.
+        """
+        limit = 10
+        filter = { "portal_type" : "Publication" }
+        # Read the first index of the selected batch parameter as HTTP GET request query parameter
+        start = self.request.get("b_start", 0)
+        # Perform portal_catalog query
+        self.contents = self.query(start, limit, filter)
+        # Return the rendered template (productcardsummaryview.pt), with content listing information filled in
+        return self.index()
+
 
 class PublicationsOneView(BrowserView):
     """Default view of a Project Folder
